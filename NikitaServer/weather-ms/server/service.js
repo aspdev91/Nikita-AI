bbc7978a44e63bc55ef2abdcb571fc1332d433c1
aspdev91 <mtgmusic1@gmail.com>
2017-01-29 18:01:00 -0500

    'Adjusted temp'

--- NikitaServer/weather-ms/server/service.js
+++ NikitaServer/weather-ms/server/service.js
@@ -3,19 +3,19 @@
 const express = require('express');
 const service = express();
 const request = require('superagent');
-const WEATHER_API_KEY = require('../env')
+const WEATHER_API_TOKEN = require('../env').WEATHER_API_TOKEN
 
 service.get('/service/:location', (req, res, next) => {
     
     request.get('http://api.openweathermap.org/data/2.5/weather?q=' + 
-    req.params.location + `&APPID=${WEATHER_API_KEY}&units=metric`, 
+    req.params.location + `&APPID=${WEATHER_API_TOKEN}&units=imperial`, 
     (err, response) => {
 
         if (err) {
             console.log(err);
             return res.sendStatus(404);
         }
-
+        console.log(response.body)
         res.json({result: `${response.body.weather[0].description} at ${response.body.main.temp} degrees`});
 
     });
